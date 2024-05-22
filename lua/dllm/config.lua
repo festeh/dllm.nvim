a0e9b6a533260a761fba76d4755f7d4c6bd7bd9d
Dmitrii Lipin <dimich3d@ya.ru>
2024-05-22 18:31:13 +0200

    init config

---
+++ lua/dllm/config.lua
@@ -0,0 +1,14 @@
+
+
+
+return {
+  default_provider = "anthropic",
+  temperature = 0.0,
+  max_tokens = 3000,
+  openai = {
+    default_model = "gpt-4"
+  },
+  anthropic = {
+    default_model = "claude-3"
+  }
+}
