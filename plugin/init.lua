216fc78a169e0b030954baaeed2f71f1446b7aa2
Dima Lipin <dimich3d@ya.ru>
2024-05-25 12:18:24 +0200

    init commands

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,12 +2,23 @@ local Chat = require('dllm.chat')
 local config = require('dllm.config')
 
 
-vim.api.create_user_command("dnewchat",
-  function(args)
-    Chat:create_file(config)
+vim.api.create_user_command("lmnewchat",
+  function(_)
+    Chat.create_file(config)
   end,
   {
     desc = "Create a new chat file",
+    force = true,
+  }
+)
+
+vim.api.create_user_command("lmrespond",
+  function(opts)
+    local chat = Chat.new(config)
+    chat:respond(opts)
+  end,
+  {
+    desc = "Get response from LLM provider using last N messages as a context",
     force = true,
   }
 )
